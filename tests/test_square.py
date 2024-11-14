5ab7962c7c3bbf90080eb6577cc459aaa02c676d
W1k1 <yurakiv05@gmail.com>
2024-11-15 02:53:38 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,4 +1,7 @@
 import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../geometric_lib')))
 from .square import square_area, square_perimeter
 
 def test_square_area():
