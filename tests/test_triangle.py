8728ff6cb853f86a0d9ae35b2c992be4efb6b756
W1k1 <yurakiv05@gmail.com>
2024-11-15 02:55:39 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../geometric_lib')))
-from .triangle import triangle_area, triangle_perimeter
+from geometric_lib.triangle import triangle_area, triangle_perimeter
 
 def test_triangle_area():
     assert triangle_area(3, 4, 5) == 6
