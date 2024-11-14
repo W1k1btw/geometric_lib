4dde7ad05323078b54d130cc860bf65daf3c928d
W1k1 <yurakiv05@gmail.com>
2024-11-15 02:49:19 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,5 +1,5 @@
 import pytest
-from geometric_lib.circle import circle_area, circle_perimeter
+from .circle import circle_area, circle_perimeter
 
 def test_circle_area():
     assert pytest.approx(circle_area(3), 0.01) == 28.27
