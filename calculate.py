a8365301d05d3f57ce9cf20deacfd0f7c2834386
W1k1 <yurakiv05@gmail.com>
2024-11-15 03:34:12 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -6,21 +6,10 @@ figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
 
 def calc(fig, func, size):
-    assert fig in figs
-    assert func in funcs
-
-    if fig == 'circle':
-        if func == 'area':
-            return circle.circle_area(*size)
-        elif func == 'perimeter':
-            return circle.circle_perimeter(*size)
-    elif fig == 'square':
-        if func == 'area':
-            return square.square_area(*size)
-        elif func == 'perimeter':
-            return square.square_perimeter(*size)
-    elif fig == 'triangle':
-        if func == 'area':
-            return triangle.triangle_area(*size)
-        elif func == 'perimeter':
-            return triangle.triangle_perimeter(*size)
+    if fig not in figs:
+        raise ValueError("Unsupported figure")
+    if func not in funcs:
+        raise ValueError("Unsupported function")
+    
+    result = eval(f'{fig}.{func}(*{size})')
+    return result
