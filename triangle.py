4e1f0811901a943635dd83f8fb666e65ae376932
W1k1 <yurakiv05@gmail.com>
2024-11-15 03:37:56 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,6 +1,6 @@
-def triangle_area(a, b, c):
+def area(a, b, c):
     s = (a + b + c) / 2
     return (s * (s - a) * (s - b) * (s - c)) ** 0.5
 
-def triangle_perimeter(a, b, c):
+def perimeter(a, b, c):
     return a + b + c
