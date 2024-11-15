fccaa6db717047dc4ddaaa204e78d8dd7ef88bd2
W1k1 <yurakiv05@gmail.com>
2024-11-15 03:15:41 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../geometric_lib')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from geometric_lib.circle import circle_area, circle_perimeter
 
 def test_circle_area():
