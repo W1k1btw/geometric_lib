16c0ad93291c10dae87d3561be5fd1af45d6429a
W1k1 <yurakiv05@gmail.com>
2024-11-15 03:15:49 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 import os
-ys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from geometric_lib.calculate import calc
 
 def test_calc_area():
