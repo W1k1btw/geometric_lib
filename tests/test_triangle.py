6918161e4ee06160d6065efa6f76043d4f433dee
W1k1 <yurakiv05@gmail.com>
2024-11-15 03:16:10 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../geometric_lib')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from geometric_lib.triangle import triangle_area, triangle_perimeter
 
 def test_triangle_area():
