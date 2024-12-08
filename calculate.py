9719e7123d5b7395c169c2444a600b3f6be0c3e5
W1k1 <yurakiv05@gmail.com>
2024-12-08 16:37:23 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,15 +1,16 @@
 import circle  # noqa: F401
 import square  # noqa: F401
 
-
 figs = ["circle", "square"]
 funcs = ["perimeter", "area"]
+
 
 def calc(fig, func, size):
     assert fig in figs
     assert func in funcs
     result = eval(f"{fig}.{func}(*{size})")
     return result
+
 
 if __name__ == "__main__":
     fig = input(f"Enter figure name (available: {figs}):\n").strip()
