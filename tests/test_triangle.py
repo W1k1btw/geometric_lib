a3fc63b63b519abac1617a70645edc1fd8a19a0c
W1k1 <yurakiv05@gmail.com>
2024-12-08 16:24:18 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -5,6 +5,7 @@ def test_area():
     assert area(3, 4, 5) == 6
     assert area(6, 8, 10) == 12
 
+
 def test_area_invalid():
     with pytest.raises(TypeError):
         area("Not a number.", 4, 5)
@@ -13,9 +14,11 @@ def test_area_invalid():
     with pytest.raises(TypeError):
         area(3, 4, "Not a number.")
 
+
 def test_perimeter():
     assert perimeter(3, 4, 5) == 12
     assert perimeter(6, 8, 10) == 24
+
 
 def test_perimeter_invalid():
     with pytest.raises(TypeError):
@@ -24,3 +27,4 @@ def test_perimeter_invalid():
         perimeter(3, "Not a number.", 5)
     with pytest.raises(TypeError):
         perimeter(3, 4, "Not a number.")
+
