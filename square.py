9db4a75103e1bdaa70f247fd87963a010a48f617
W1k1 <yurakiv05@gmail.com>
2024-12-08 15:58:46 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,5 +1,7 @@
 def area(a):
-    return a * a
+    return a ** 2
 
 def perimeter(a):
+    if not isinstance(a, (int, float)):
+        raise TypeError("Argument 'a' must be a number.")
     return 4 * a
